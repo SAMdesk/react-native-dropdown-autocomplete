1afc413144bb29126df48824fff2782989c02d19
Jordan Ching <ching@samdesk.io>
2021-03-12 13:31:02 -0700

    fix outdated lifecycle method warning componentWillReceiveProps to getDerivedStateFromProps

--- components/Dropdown/index.js
+++ components/Dropdown/index.js
@@ -52,11 +52,13 @@ export default class Dropdown extends PureComponent {
     this.renderHeader = this.renderHeader.bind(this);
   }
 
-  componentWillReceiveProps(nextProps) {
-    const {value} = this.props;
-    if (nextProps.value !== value) {
-      this.setState({value: nextProps.value});
+  static getDerivedStateFromProps(props, state) {
+    if (props.value !== state.value) {
+      return {
+        value: props.value,
+      };
     }
+    return null;
   }
 
   componentDidMount() {
